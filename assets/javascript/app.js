58fec271717af062105403c8780b76109e615bb2
SPallai <sangeeta.sumair@gmail.com>
2018-12-09 17:06:19 -0800

    updated recipe URL

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,12 @@
+var recipeBaseUrl = "https://api.edamam.com/search?&app_id=c3d07cfd&app_key=2a58703995fceba093b9313f0c1ee00e&from=0&to=3&calories=591-722&health=alcohol-free&q="
+console.log(recipeBaseUrl);
 
+var recipeTerm = "";
 
-
-$("#search").on("click", function() {
-
+$("#submit").on("click", function(event) {
+    event.preventDefault();
+    recipeTerm = $("#search").val().trim();
+    var recipeSearchURL = recipeBaseUrl + recipeTerm;
+    console.log(recipeSearchURL);
 });
 
