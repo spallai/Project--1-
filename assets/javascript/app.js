c90f710546c58b49c69e046a4544386461eab4f6
clintforster <clinte.forster@gmail.com>
2018-12-12 12:53:00 -0800

    made comments for presentation on app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,20 +6,34 @@ var lng;
 var markers = [];
 
 
+
 initMap();
 getLocation();
 
+
+
+// Creates initial map that renders as soon as the page loads.
+// Los Angeles is the initial center point before the maps/places api's ask to enable current-location services.
+// initial zoom level is 13.
+
+
 function initMap() {
 
-    var myLocation = { lat: 34, lng: -118 };
+    var myLocation = { lat: 34.0522, lng: -118.2437};
 
     map = new google.maps.Map(document.getElementById('map'), {
-        center: new google.maps.LatLng(34.0522, -118.2437),
+        center: myLocation,
         zoom: 13
     });
 
 }
 
+
+
+// getLocation() checks to see if the user allowed us to use the current-location services.
+// If they have allowed us to use it it will get the users current position and uses the showPosition 
+// function to set the center of the map to the current position cordinates.
+// If the user has not allowed us to use their current location, the map will never center itself over the users location.
 
 function getLocation() {
     if (navigator.geolocation) {
@@ -28,6 +42,7 @@ function getLocation() {
         alert("Geolocation is not supported by this browser.");
     }
 }
+
 function showPosition(position) {
     lat = position.coords.latitude;
     lng = position.coords.longitude;
@@ -35,18 +50,18 @@ function showPosition(position) {
 }
 
 
+
+// We use a document.on("click") function in order 
+
 $(document).on("click", "#submit", function() {
-    var newSearch = $("#search").val(); //read text from input box #search
-    searchMap(newSearch); //pass search term to search map
+    var newSearch = $("#search").val();
+    searchMap(newSearch);
 });
 
 
-//turned searchMap into a function that can be used in recipeLogic
+
 function searchMap(newSearch) {
     event.preventDefault();
-    //var newSearch = $("#search").val();
-    console.log(newSearch);
-
     var request = {
         location: new google.maps.LatLng(lat, lng),
         query: newSearch,
@@ -56,14 +71,17 @@ function searchMap(newSearch) {
 
     service = new google.maps.places.PlacesService(map);
     service.textSearch(request, callback);
-
 }
+
+
 
 function setMapOnAll(map) {
     for (var i = 0; i < markers.length; i++) {
         markers[i].setMap(map);
     }
 }
+
+
 
 function deleteMarkers() {
     for (var i = 0; i < markers.length; i++) {
@@ -72,12 +90,12 @@ function deleteMarkers() {
     markers = [];
 }
 
+
+
 $(document).on("click", "#search", function () {
-
     deleteMarkers();
-    
-
 })
+
 
 
 function createMarker(place) {
@@ -94,6 +112,7 @@ function createMarker(place) {
             address: place.formatted_address,
             rating: place.rating
         }
+
         console.log(content);
         var stringAddress = content.address;
         var stringName = content.name;
@@ -112,7 +131,6 @@ function callback(results, status) {
         for (var i = 0; i < results.length; i++) {
             var place = results[i];
             createMarker(results[i]);
-
         }
     }
 }
